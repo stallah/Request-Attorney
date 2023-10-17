a2b4bacad83aad6ed861e936de41e397b5f95180
stallah <smita.tallah@gmail.com>
2023-10-17 18:53:14 -0400

    added shell of class

--- RequestAttorneyAppointment.java
+++ RequestAttorneyAppointment.java
@@ -1,7 +1,32 @@
 public class RequestAttorneyAppointment {
+    private int requestFormNumber;
+    private String appointmentDetail;
+
     public RequestAttorneyAppointment(){
+        this.appointmentDetail = null;
+        this.requestFormNumber = 0;
+    }
+    public RequestAttorneyAppointment(String appointmentDetail, int requestFormNumber){
+        this.requestFormNumber = requestFormNumber;
+        this.appointmentDetail = appointmentDetail;
+    }
+
+    public boolean acceptRequest(int requestFormNumber){
+        return false;
+    }
+
+    public boolean rejectRequest(int requestFormNumber){
+        return false;
+    }
+
+    public void updateAvailability(String newAvailability){
 
     }
+
+    public void appointmentDetails(int requestFormNumber){
+
+    }
+
     public String toString(){
         return "hello world";
     }
