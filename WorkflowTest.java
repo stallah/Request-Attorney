d03764f8fe8a0bcef9ec742b10c68ba1745e443f
ashnat7 <ashnat7@gmail.com>
2023-10-18 18:44:23 -0400

    comments

--- WorkflowTest.java
+++ WorkflowTest.java
@@ -21,7 +21,9 @@ public class WorkflowTest {
     public void testSendRequestForm() {
         Workflow wf = Workflow.createWorkflow();
         Assert.assertNotNull(wf);
+        //testing if form number is equal
         Assert.assertEquals(0, wf.sendForm(0));
+        //testing if form number is a positive number
         Assert.assertTrue(wf.sendForm(0)>0);
 
     }
@@ -31,6 +33,7 @@ public class WorkflowTest {
     public void testAttorneyResponse() {
         Workflow wf = Workflow.createWorkflow();
         Assert.assertNotNull(wf);
+        //testing for attorney response
         Assert.assertTrue(wf.sendAttResponse(0));
     }
 
@@ -39,6 +42,7 @@ public class WorkflowTest {
     public void testSaveAttorneyResponse() {
         Workflow wf = Workflow.createWorkflow();
         Assert.assertNotNull(wf);
+        //testing if attorney details are saved successfully
         Assert.assertTrue(wf.saveAttDetails(null));
     }
 }
