10ebb862af8825ecc9030eaccf72fbac575bec68
stallah <smita.tallah@gmail.com>
2023-10-30 17:45:55 -0400

    new screen

--- Request-attorney1/src/ApproverS.java
+++ Request-attorney1/src/ApproverS.java
@@ -10,6 +10,9 @@ import javax.swing.border.TitledBorder;
 import java.awt.Color;
 import java.awt.TextField;
 import java.awt.Panel;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ApproverS {
 
@@ -45,13 +48,26 @@ public class ApproverS {
 		frmApprover = new JFrame();
 		frmApprover.getContentPane().setBackground(Color.WHITE);
 		
-		TextField textField = new TextField();
-		textField.setText("Enter Form Number");
-		textField.setBackground(Color.LIGHT_GRAY);
-		frmApprover.getContentPane().add(textField, BorderLayout.NORTH);
-		
 		Panel panel = new Panel();
+		panel.setBackground(Color.LIGHT_GRAY);
 		frmApprover.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JTextPane txtpnEnterFormNumber = new JTextPane();
+		txtpnEnterFormNumber.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtpnEnterFormNumber.setBackground(Color.LIGHT_GRAY);
+		txtpnEnterFormNumber.setBounds(10, 60, 150, 20);
+		txtpnEnterFormNumber.setText("Enter Form Number:");
+		panel.add(txtpnEnterFormNumber);
+		
+		TextField textField = new TextField();
+		textField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//call sendRequestForm
+			}
+		});
+		textField.setBounds(166, 59, 154, 21);
+		panel.add(textField);
 		frmApprover.setTitle(" Approver Screen");
 		frmApprover.setBounds(100, 100, 450, 300);
 		frmApprover.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
