678976d9e75768c5d11e45d6a9d3fd88dc635cc3
ashnat7 <ashnat7@gmail.com>
2023-11-03 11:52:27 -0400

    data entry screen

--- Request-attorney1/src/DataEntryS.java
+++ Request-attorney1/src/DataEntryS.java
@@ -15,11 +15,22 @@ import javax.swing.JOptionPane;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import java.awt.Button;
+import java.awt.Dimension;
 
 public class DataEntryS {
 
 	private JFrame frmDataEntry;
 	private JTextField textField;
+	private final Action action = new SwingAction();
+	
 
 	/**
 	 * Launch the application.
@@ -42,6 +53,7 @@ public class DataEntryS {
 	 */
 	public DataEntryS() {
 		initialize();
+
 	}
 
 	/**
@@ -54,7 +66,7 @@ public class DataEntryS {
 		frmDataEntry.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmDataEntry.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Enter Name:");
+		JLabel lblNewLabel = new JLabel("Attorney ID:");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel.setBounds(23, 32, 100, 23);
 		frmDataEntry.getContentPane().add(lblNewLabel);
@@ -62,11 +74,74 @@ public class DataEntryS {
 		textField = new JTextField();
 		textField.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Getting information...");
+				System.out.println(textField.getText());
+				//RequestAttorneyAppointment.createRequestAttorneyAppointment("11/21/23", 001);
+				//JOptionPane.showMessageDialog(null, "Getting information...");
+				
+				//call from workflow
 			}
 		});
 		textField.setBounds(133, 35, 96, 20);
 		frmDataEntry.getContentPane().add(textField);
 		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Submit");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				new Screen2();
+				Screen2 sc2 = new Screen2();
+				//screen2 = new JFrame();
+				//sc2.show();
+				sc2.setBounds(100, 100, 450, 300);
+				sc2.create_appt.setVisible(true);
+				sc2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				sc2.getContentPane().setLayout(null);
+				frmDataEntry.dispose();				
+			}
+		});
+		
+		btnNewButton.setBounds(140, 86, 89, 23);
+		frmDataEntry.getContentPane().add(btnNewButton);
+	}
+	
+	public boolean newAttorneyDetail(String newAttDetail) {
+		//save new attorney detail
+		return true;
+	}
+	
+	public boolean updateAttorneyDetail(String modAttDetail){
+		//when updated detail is saved
+		return true;
+		
+	}
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
+	private class SwingAction extends AbstractAction {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
 	}
 }
